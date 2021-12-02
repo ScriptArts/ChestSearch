ccda6914a88f4e9adb158d858f3e2876dc9565c6
ハル <tatuharu.lbp@gmail.com>
2021-12-02 09:24:25 +0900

    v1.0開発

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # EmptyChestSearch
-アイテムが格納されていないチェストやかまど等のアイテムを格納することができるタイルエンティティの座標を検索するAmuletEditor用プラグインです。
+Amulet Editor用プラグイン  
+アイテムが格納されていないチェストやかまど等のアイテムを格納することができるタイルエンティティの座標を検索する
+
+## 配置場所
+AmuletEditorの`plugins/operations`に配置してください。
+
+## 検索方法
+1.AmuletEditorを起動し、3DEditorのoperationsから`空チェスト検索`を選択する。
+
+2.`検索開始`ボタンを押下する。
+
+3.検索が完了するまで待つ。
+
+4.検索が完了したら、AmuletEditorの実行ファイルと同じ階層のディレクトリにEmptyChestSearchのディレクトリが作成されます。
+
+5.EmptyChestSearchディレクトリに`YYYYMMDDHHMMSS.csv`の形式でCSVが出力されます。（検索を開始した日時）
---
+++ empty_chest_search.py
@@ -0,0 +1,128 @@
+import os
+from datetime import datetime
+
+import numpy
+from amulet import Block, SelectionBox
+from typing import TYPE_CHECKING, Tuple, Dict
+import wx
+from amulet.api.partial_3d_array.base_partial_3d_array import BasePartial3DArray
+
+from amulet_map_editor.api.wx.ui.base_select import EVT_PICK
+from amulet_map_editor.api.wx.ui.block_select import BlockDefine
+from amulet_map_editor.programs.edit.api.operations import DefaultOperationUI
+
+if TYPE_CHECKING:
+    from amulet.api.level import BaseLevel
+    from amulet_map_editor.programs.edit.api.canvas import EditCanvas
+
+
+def _check_block(block: Block, original_base_name: str,
+                 original_properties: Dict[str, "WildcardSNBTType"]) -> bool:
+    if (block.base_name == original_base_name
+            and all(
+                original_properties.get(prop) in ["*", val.to_snbt()]
+                for prop, val in block.properties.items()
+            )
+    ):
+        return True
+    return False
+
+
+class EmptyChestSearch(wx.Panel, DefaultOperationUI):
+    def __init__(
+            self, parent: wx.Window, canvas: "EditCanvas", world: "BaseLevel", options_path: str
+    ):
+        wx.Panel.__init__(self, parent)
+        DefaultOperationUI.__init__(self, parent, canvas, world, options_path)
+
+        self.Freeze()
+        self._sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self._sizer)
+
+        options = self._load_options({})
+
+        self._description = wx.TextCtrl(
+            self, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_BESTWRAP, size=(200, 100)
+        )
+        self._sizer.Add(self._description, 0, wx.ALL | wx.EXPAND, 5)
+        self._description.SetLabel("アイテムが格納可能なタイルエンティティのうち、\n" +
+                                   "アイテムが格納されていないタイルエンティティをワールドから全て検索します。\n" +
+                                   "検索した内容はEmptyChestSearchディレクトリにCSV形式で出力します。")
+        self._description.Fit()
+
+        self._run_button = wx.Button(self, label="検索開始")
+        self._run_button.Bind(wx.EVT_BUTTON, self._run_operation)
+        self._sizer.Add(self._run_button, 0, wx.ALL | wx.ALIGN_CENTRE_HORIZONTAL, 5)
+
+        self.Layout()
+        self.Thaw()
+
+    @property
+    def wx_add_options(self) -> Tuple[int, ...]:
+        return (1,)
+
+    def _on_pick_block_button(self, evt):
+        self._show_pointer = True
+
+    def disable(self):
+        print("Unload EmptyChestSearch")
+
+    def _run_operation(self, _):
+        self.canvas.run_operation(
+            lambda: self._find_block()
+        )
+
+    def _find_block(self):
+        world = self.world
+        chunk_count = 0
+        count = 0
+        file_out_list = []
+        now = datetime.now()
+        directory_name = "EmptyChestSearch"
+        filepath = directory_name + "/" + now.strftime("%Y%m%d%H%M%S") + ".csv"
+
+        # 全てのディメンションのチャンク数を取得
+        for dimension in world.dimensions:
+            chunk_count += len(list(world.all_chunk_coords(dimension)))
+
+        print("ブロック検索プラグイン実行")
+        print("総検索チャンク数:" + str(chunk_count))
+        print("----------検索開始----------")
+
+        for dimension in world.dimensions:
+            for cx, cz in world.all_chunk_coords(dimension):
+                chunk = world.get_chunk(cx, cz, dimension)
+
+                for blockEntity in chunk.block_entities:
+                    snbt = blockEntity.nbt.to_snbt()
+                    if "Items: []" in snbt:
+                        x = str(blockEntity.x)
+                        y = str(blockEntity.y)
+                        z = str(blockEntity.z)
+                        base_name = blockEntity.base_name
+
+                        print("X:" + x + " Y:" + y + " Z:" + z + " " + dimension + " " + base_name + " " + snbt)
+                        file_out_list.append((x, y, z, dimension, base_name))
+
+                count += 1
+                yield count / chunk_count
+
+        print("----------検索終了----------")
+        print("検索結果出力 -> " + filepath)
+
+        # ディレクトリ生成
+        os.makedirs(directory_name, exist_ok=True)
+
+        # 結果をファイル出力
+        with open(filepath, "w") as f:
+            f.write("x,y,z,dimension,block_name\n")
+            for x, y, z, dimension, base_name, in file_out_list:
+                f.write(x + "," + y + "," + z + "," + dimension + "," + base_name + "\n")
+
+        wx.MessageBox("検索が完了しました。\n出力先：" + filepath, "検索完了")
+
+
+export = {
+    "name": "空チェスト検索",  # the name of the plugin
+    "operation": EmptyChestSearch,  # the actual function to call when running the plugin
+}
